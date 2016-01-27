96377b15cb0feaf2d4b7e8f6cf8f1f43b0de89de
Robert Hedgpeth <robert.hedgpeth@xamarin.com>
2016-01-27 11:38:21 -0600

    Initial add

---
+++ Braintree.DropIn/Additions/PaymentButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Runtime;
+
+namespace Com.Braintreepayments.Api {
+
+    public partial class PaymentButton  {
+        public void OnResponse(Java.Lang.Object obj)
+        {
+
+        }
+    }
+}
---
+++ BraintreeV2Example/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Braintreepayments.Api;
+using System;
+
+namespace BraintreeV2Example
+{
+    [Activity (
+        Label = "BraintreeV2Example",
+        MainLauncher = true,
+        Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        BraintreeFragment mBraintreeFragment;
+
+        protected override void OnCreate (Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+
+            try {
+                mBraintreeFragment = BraintreeFragment.NewInstance(this, "token");
+                // mBraintreeFragment is ready to use!
+            } catch (Exception e) {
+                Console.WriteLine (e);
+            }
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+            button.Click += delegate
+            {
+                
+            };
+        }
+    }
+}
+
+
